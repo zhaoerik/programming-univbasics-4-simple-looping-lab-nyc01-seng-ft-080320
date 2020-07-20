8d60dcec1b30eb08aaac9b551edb72c15bb3c09f
Erik Zhao <erikzhao98@gmail.com>
2020-07-20 18:03:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,24 +7,24 @@
 #   end
 # end
 #
-# def loop_messages_n_times(string, limit)
-#   count = 0
-#   while count < limit do
-#     puts string
-#     count += 1
-#   end
-# end
+def loop_message_n_times(string, limit)
+  count = 0
+  while count < limit do
+    puts string
+    count += 1
+  end
+end
 #
 # def output_array(array)
 #   puts array
 # end
 
-def return_string_array(array)
-  count = 0
-  new_array = []
-  while count < array.length do
-    new_array.push(array[count].to_s)
-    count += 1
-  end
-  new_array
-end
+# def return_string_array(array)
+#   count = 0
+#   new_array = []
+#   while count < array.length do
+#     new_array.push(array[count].to_s)
+#     count += 1
+#   end
+#   new_array
+# end
