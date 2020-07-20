9ec15df008825b780e9f3c221dd93be2f9aba66a
Erik Zhao <erikzhao98@gmail.com>
2020-07-20 17:42:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,11 @@
-# # Write your methods here
-# def loop_message_five_times(string)
-#   count = 0
-#   while count < 5 do
-#     puts string
-#     count += 1
-#   end
-# end
+# Write your methods here
+def loop_message_five_times(string)
+  count = 0
+  while count < 5 do
+    puts string
+    count += 1
+  end
+end
 
 def loop_messages_n_times(string, limit)
   count = 0
