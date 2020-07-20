7aeaf642b8f5912c6d8bae662e1bcedba8619872
Erik Zhao <erikzhao98@gmail.com>
2020-07-20 17:46:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,8 +21,10 @@
 
 def return_string_array(array)
   count = 0
+  new_array = []
   while count < array.length do
-    p array
+    new_array.push(array[count].to_s)
     count += 1
   end
+  new_array
 end
